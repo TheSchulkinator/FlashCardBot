df7cd76e43845df4fdf12c47dcc093cbaa5e0682
corecritter <bobbysavel@gmail.com>
2017-07-04 23:23:11 -0400

    Changed so FunctionHandler receives a dynamic type and uses the IntentName to create an IntentHandler
    Kind of a mess right now

---
+++ ChatBotHook.IntentHandlers/FlashCardBotIntentCreator.cs
@@ -0,0 +1,21 @@
+﻿using Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBotHook.IntentHandlers
+{
+    public class FlashCardBotIntentCreator : IntentCreator
+    {
+        public override IIntentHandler CreateIntentHandler(string intentName)
+        {
+            IIntentHandler intentHandler = null;
+            if (intentName == "addorrecognizeuser")
+                intentHandler = new AddOrRecognizeHandler();
+            if(intentName == "bookcar")
+                intentHandler = new AddOrRecognizeHandler();
+
+            return intentHandler;
+        }
+    }
+}
---
+++ ChatBotHook.IntentHandlers/IIntentHandler.cs
@@ -0,0 +1,17 @@
+﻿using Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatBotHook.IntentHandlers
+{
+    public interface IIntentHandler<SlotType> : IIntentHandler where SlotType : BaseSlotType 
+    {
+        InputModel<SlotType> InputModel { get; set; }
+    }
+
+    public interface IIntentHandler
+    {
+        dynamic HandleIntent(dynamic inputModel);
+    }
+}
