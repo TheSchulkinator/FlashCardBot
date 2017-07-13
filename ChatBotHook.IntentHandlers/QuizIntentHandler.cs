c1357734c9729c94a325a020b2d279e70ce34da6
Unknown <Gregschulkey@gmail.com>
2017-07-12 21:35:22 -0400

    Some quiz work

--- ChatBotHook.IntentHandlers/QuizIntentHandler.cs
+++ ChatBotHook.IntentHandlers/QuizIntentHandler.cs
@@ -10,7 +10,27 @@ namespace ChatBotHook.IntentHandlers
     {
         protected override OutputModel<QuizSlotType> ProcessIntent()
         {
-            var outputModel = new OutputModel<QuizSlotType>();
+            string fulfillmentState = null;
+            string intentName = InputModel.CurrentIntent.Name;
+            var slots = InputModel.CurrentIntent.Slots;
+            var errors = InputModel.CurrentIntent.Slots.Validate();
+            string slotToElicit = InputModel.CurrentIntent.Slots.GetSlotToElicit();
+            string responseMessage = String.Empty;
+
+            if(slotToElicit == nameof(QuizSlotType.DeckName))
+            {
+                responseMessage = "What is the name of the deck you'd like to be quizzed on?";
+            }
+            else if (slotToElicit == nameof(QuizSlotType.QuizOrder))
+            {
+                responseMessage = "Would you liked to be quizzed in a sequential or random fashion?";
+            }
+            else if (slotToElicit == nameof(QuizSlotType.QuizProgression))
+            {
+                
+            }
+
+                var outputModel = new OutputModel<QuizSlotType>();
 
             outputModel.dialogAction.message.content = "Let the quiz begin";
             outputModel.dialogAction.intentName = Constants.INTENT_NAME_QUIZ;
